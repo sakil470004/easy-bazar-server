108457f86ae0e8ceaa578d062159635eea150c1e
Mynul Islam <mynul.sakil@gmail.com>
2024-06-04 22:11:45 +0600

    chore: Refactor index.js to include MongoDB connection configuration

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ const client = new MongoClient(uri, {
   },
 });
 
-
 async function run() {
   try {
     client.connect();
@@ -60,13 +59,13 @@ async function run() {
     });
 
     // product routes
-    app.post("/products",verifyToken, async (req, res) => {
+    app.post("/products", verifyToken, async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
 
       res.json(result);
     });
-    app.patch("/products/:id",verifyToken, async (req, res) => {
+    app.patch("/products/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const updatedProduct = req.body;
       const { _id, ...restUpdate } = updatedProduct;
@@ -82,7 +81,7 @@ async function run() {
       );
       res.json(result);
     });
-    app.delete("/products/:id",verifyToken, async (req, res) => {
+    app.delete("/products/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const result = await productCollection.deleteOne({
         _id: new ObjectId(id),
@@ -114,7 +113,7 @@ async function run() {
       }
       const existingUser = await userCollection.findOne(filter);
       if (existingUser) {
-        res.status(400).send({ status: "Success Email already exists",token });
+        res.status(400).send({ status: "Success Email already exists", token });
         return;
       }
       const result = await userCollection.insertOne(user);
@@ -130,7 +129,7 @@ async function run() {
       const user = await userCollection.findOne({ email: email });
       res.json(user);
     });
-    app.patch("/users/:email", async (req, res) => {
+    app.patch("/users/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       const updatedUser = req.body;
       const { _id, ...restUpdate } = updatedUser;
@@ -143,65 +142,7 @@ async function run() {
       res.json(result);
     });
 
-    // // product routes
-    // app.post("/shoes", async (req, res) => {
-    //   const shoe = req.body;
-    //   const result = await shoeCollection.insertOne(shoe);
-    //   res.json(result);
-    // });
-    // // get all products
-    // app.get("/shoes", async (req, res) => {
-    //   const cursor = shoeCollection.find({});
-    //   const shoes = await cursor.toArray();
-    //   res.json(shoes);
-    // });
-    // // get single product
-    // app.get("/shoes/:id", async (req, res) => {
-    //   const id = req.params.id;
-
-    //   const shoe = await shoeCollection.findOne({ _id: new ObjectId(id) });
-    //   res.json(shoe);
-    // });
-    // app.patch("/shoes/:id", verifyToken, async (req, res) => {
-    //   const id = req.params.id;
-    //   const updatedShoe = req.body;
-    //   const { _id, ...restUpdate } = updatedShoe;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: restUpdate,
-    //   };
-    //   const result = await shoeCollection.updateOne(filter, updateDoc, options);
-    //   res.json(result);
-    // });
-    // app.delete("/shoes/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const result = await shoeCollection.deleteOne({ _id: new ObjectId(id) });
-    //   res.json(result);
-    // });
-    // // user routes
-    // app.post("/users", async (req, res) => {
-    //   const user = req.body;
-    //   const token = createToken(user);
-    //   const filter = { email: user.email };
-    //   if (user.email === "") {
-    //     res.status(400).send({ status: "Email  can not be empty" });
-    //     return;
-    //   }
-    //   const existingUser = await userCollection.findOne(filter);
-    //   if (existingUser) {
-    //     res.status(400).send({ status: "Success Email already exists", token });
-    //     return;
-    //   }
-    //   const result = await userCollection.insertOne(user);
-    //   res.json({ token, status: "Success add user to" });
-    // });
-    // // get all users
-    // app.get("/users", async (req, res) => {
-    //   const cursor = userCollection.find({});
-    //   const users = await cursor.toArray();
-    //   res.json(users);
-    // });
+    
   } finally {
     // await client.close();
   }
